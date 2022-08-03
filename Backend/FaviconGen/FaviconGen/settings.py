5be3e38e0e0ef8db8e6f23abb1786aeeb0075791
Karen Hervie <akuhervie02@gmail.com>
2022-08-03 18:56:32 +0100

    favitude

--- Backend/FaviconGen/FaviconGen/settings.py
+++ Backend/FaviconGen/FaviconGen/settings.py
@@ -142,3 +142,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_PROFILE_MODULE = 'users.MyUser'
+
+AUTH_USER_MODEL = 'users.MyUser'
--- Backend/FaviconGen/Favitude/models.py
+++ Backend/FaviconGen/Favitude/models.py
@@ -1,15 +1,26 @@
 import email
 from unicodedata import name
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 from django.forms import modelform_factory
 
 # Create your models here.
-class User(models.Model) :
-   user_id = models.serial
-   first_name = models.CharField(max_length=100)
-   last_name = models.CharField(max_length=100)
-   email = models.CharField(max_length=100) 
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
 
-class Avatar(models.Model) :
-    user_id = models.serial
-    avatar_id = models.int
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
