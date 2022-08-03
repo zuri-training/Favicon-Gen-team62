9fbab2e0da47382539c93eb2f82a77412095d6c6
Allan <allanmuli18@gmail.com>
2022-08-03 23:08:49 +0300

    Update settings.py

--- Backend/FaviconGen/FaviconGen/settings.py
+++ Backend/FaviconGen/FaviconGen/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Favitude'
     
 ]
 
@@ -96,8 +97,12 @@ WSGI_APPLICATION = 'FaviconGen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'FavitudeDB',
+        'USER': 'postgres',
+        'PASSWORD': 'infamousal',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
