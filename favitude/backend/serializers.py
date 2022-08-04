c74eb06b715f45388833f8914cb4188d8586bc1a
Karen Hervie <akuhervie02@gmail.com>
2022-08-04 20:10:41 +0100

    Delete favitude directory

--- favitude/backend/serializers.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import fields
-from pyexpat import model
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'first_name', 'last_name')
