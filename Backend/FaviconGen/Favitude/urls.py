92c5aff5ba81590eeed57e3844501fda6da2560b
HILLARY GOODNESS <34764510+africreativ@users.noreply.github.com>
2022-08-09 20:17:23 +0100

    Add home templates page and render views, url

--- Backend/FaviconGen/Favitude/urls.py
+++ Backend/FaviconGen/Favitude/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path("", views.home, name="home_page"),
   path("about/", views.about, name="about_page"),
-  path("login/", views.login, name="login_page"),
-  path("signup/", views.signup, name="signup_page"),
+  path("login/", views.login_page, name="login_page"),
+  path("signup/", views.signup_page, name="signup_page"),
 ]
--- Backend/FaviconGen/Favitude/views.py
+++ Backend/FaviconGen/Favitude/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+  return render(request, 'Favitude/index.html')  
+
 def about(request):
   return render(request, 'Favitude/about.html')
 
 
-def login(request):
+def login_page(request):
   return render(request, 'Favitude/login.html')
 
 
-def signup(request):
+def signup_page(request):
   return render(request, 'Favitude/signup.html')  
