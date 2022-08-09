16f331c38015fce413d15257166afb6d27bb2684
HILLARY GOODNESS <34764510+africreativ@users.noreply.github.com>
2022-08-09 14:57:53 +0100

    Add django template for About page

--- Backend/FaviconGen/Favitude/views.py
+++ Backend/FaviconGen/Favitude/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def about(request):
-  return render(request, 'Favitude/about_page.html')
+  return render(request, 'Favitude/about.html')
